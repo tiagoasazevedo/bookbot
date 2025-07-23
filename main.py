69e3208b833bfd70e47be60557c66590813c5625
tiagoasazevedo <azevedosatiago@gmail.com>
2025-07-23 11:40:48 +0100

    Added a function that counts the individual characters in a dcoument

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ def get_book_text(book): #function that will open and fetch the document content
 	return file_contents #returns the value so it can be used
 
 from stats import count_words #imports the fucntion stored in the stats.py file
+from stats import count_each_charater #imports the function in the stats.py file
 
 def main(): #the funcion that will do the work to communicate the final results
 	book_content = get_book_text("books/frankenstein.txt") #it uses the function that will fetch the document and tells it which document to use
 	num_words = count_words(book_content) #counts the words and stores the value in a variable to later usage
+	character_counts = count_each_charater(book_content) #counts the character and stores the value in a variable to later usage
 	print(f"{num_words} words found in the document") #it prints the result with the desired message
-
+	print(character_counts)
 main() #calls the function that communicates the desired end result
--- stats.py
+++ stats.py
@@ -2,3 +2,9 @@ def count_words(file_contents): #function that will count the words
 	words = file_contents.split() #splits the contents of the document and adds every word to a list
 	num_words = len(words) #counts the items in the list and stores in a variable
 	return num_words #it returns the value so it can be later used
+
+def count_each_charater(file_contents): #function that will count each charater recurrence
+	import collections #imports collections so I can use them
+	lower_contents = file_contents.lower() #lower each character so we don't have duplicates
+	character_counts = collections.Counter(lower_contents) #uses a collection to to count the character
+	return character_counts
