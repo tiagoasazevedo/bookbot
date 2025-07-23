266357a7ea923ba3350255ca4bd12f0ea1c83aa2
tiagoasazevedo <azevedosatiago@gmail.com>
2025-07-23 10:56:36 +0100

    Added comments to better understanding of whats happening

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-def get_book_text(book):
-	with open(book) as f:
-		file_contents = f.read()
-	return file_contents
+def get_book_text(book): #function that will open and fetch the document contents
+	with open(book) as f: #opens the book and stores it temporarly in f
+		file_contents = f.read() #reads the content and stores in the new variable to be used later
+	return file_contents #returns the value so it can be used
 
-from stats import count_words
+from stats import count_words #imports the fucntion stored in the stats.py file
 
-def main():
-	book_content = get_book_text("books/frankenstein.txt")
+def main(): #the funcion that will do the work to communicate the final results
+	book_content = get_book_text("books/frankenstein.txt") #it uses the function that will fetch the document and tells it which document to use
 	num_words = count_words(book_content)
 	print(f"{num_words} words found in the document")
 
