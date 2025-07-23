965fdd1922493edfd7beba5622edd5860e9bfbfd
tiagoasazevedo <azevedosatiago@gmail.com>
2025-07-23 10:58:38 +0100

    Added comments to better understanding of what's happening

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from stats import count_words #imports the fucntion stored in the stats.py file
 
 def main(): #the funcion that will do the work to communicate the final results
 	book_content = get_book_text("books/frankenstein.txt") #it uses the function that will fetch the document and tells it which document to use
-	num_words = count_words(book_content)
-	print(f"{num_words} words found in the document")
+	num_words = count_words(book_content) #counts the words and stores the value in a variable to later usage
+	print(f"{num_words} words found in the document") #it prints the result with the desired message
 
-main()
+main() #calls the function that communicates the desired end result
--- stats.py
+++ stats.py
@@ -1,4 +1,4 @@
-def count_words(file_contents):
-	words = file_contents.split()
-	num_words = len(words)
-	return num_words
+def count_words(file_contents): #function that will count the words
+	words = file_contents.split() #splits the contents of the document and adds every word to a list
+	num_words = len(words) #counts the items in the list and stores in a variable
+	return num_words #it returns the value so it can be later used
