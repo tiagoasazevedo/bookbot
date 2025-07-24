a8a7bd3fa95899111332ee45c6cea2ca914908b1
tiagoasazevedo <azevedosatiago@gmail.com>
2025-07-24 01:03:36 +0100

    Now it is able to only show alphabetical entries but it doesn't sort.

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ from stats import sort_characters #imports the function in the stats.py file
 
 def main(): #the funcion that will do the work to communicate the final results
 	book_content = get_book_text("books/frankenstein.txt") #it uses the function that will fetch the document and tells it which document to use
-	num_words = count_words(book_content) #counts the words and stores the value in a variable to later usage
-	character_counts = sort_characters(book_content) #counts the sorted character and stores the value in a variable to later usage
+	num_words = count_words(book_content) #counts the words and stores the value in a variable for later usage
+	character_counts = count_each_charater(book_content) #counts the sorted character and stores the value in a variable for later usage
+	character_entries = sort_characters(character_counts) #sorts the characters and stores the entries in a variable for later usage
 	print(f"{num_words} words found in the document") #it prints the result with the desired message
-	print(character_counts)
+	print(character_entries)
 main() #calls the function that communicates the desired end result
