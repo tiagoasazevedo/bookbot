ff0b16e88c3804b815f6510c2f881ac2a7c1e8e3
tiagoasazevedo <azevedosatiago@gmail.com>
2025-07-25 01:36:49 +0100

    It now works as intended. It works with any given book

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from stats import count_each_charater #imports the function in the stats.py file
 from stats import sort_characters #imports the function in the stats.py file
 
 def main(): #the funcion that will do the work to communicate the final results
-	book_content = get_book_text() #it uses the function that will fetch the document and tells it which document to use
+	book_content = get_book_text(sys.argv[1]) #it uses the function that will fetch the document and tells it which document to use
 	num_words = count_words(book_content) #counts the words and stores the value in a variable for later usage
 	character_counts = count_each_charater(book_content) #counts the sorted character and stores the value in a variable for later usage
 	character_entries = sort_characters(character_counts) #sorts the characters and stores the entries in a variable for later usage
